8a9bdd12fd7af0d1bc705e9fdc056c1920ddc1cd
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-11-23 16:51:58 +0700

    add method addnode

--- exercise 3/Program.cs
+++ exercise 3/Program.cs
@@ -12,6 +12,7 @@ namespace exercise_3
         public int rollNumber;
         public string name;
         public Node next;
+        public Node prev;
     }
     class CircularList
     {
@@ -20,6 +21,57 @@ namespace exercise_3
         public CircularList()
         {
             LAST = null;
+        }
+        public void addNode()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            
+            if (LAST == null || rollNo <= LAST.rollNumber)
+            {
+                if ((LAST != null) && (rollNo == LAST.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = LAST;
+                if (LAST != null)
+                    LAST.prev = newnode;
+                newnode.prev = null;
+                LAST = newnode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = LAST; current != null &&
+                rollNo >= current.rollNumber; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+           
+            newnode.next = current;
+            newnode.prev = previous;
+
+            
+            if (current == null)
+            {
+                newnode.next = null;
+                previous.next = newnode;
+                return;
+            }
+            current.prev = newnode;
+            previous.next = newnode;
+
         }
 
         public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
@@ -50,13 +102,8 @@ namespace exercise_3
             {
                 Console.WriteLine("\nRecords in the list are:\n");
                 Node currentNode;
-                currentNode = LAST.next;
-                while (currentNode != LAST)
-                {
-                    Console.Write(currentNode.rollNumber + "    " + currentNode.name + "\n");
-                    currentNode = currentNode.next;
-                }
-                Console.Write(LAST.rollNumber + "    " + LAST.name + "\n");
+                for (currentNode = LAST; currentNode != null; currentNode = currentNode.next)
+                    Console.Write(currentNode.rollNumber + "  " + currentNode.name + "\n");
             }
         }
         public void firstNode()
@@ -74,6 +121,7 @@ namespace exercise_3
                 try
                 {
                     Console.WriteLine("\nMenu");
+                    Console.WriteLine("5. add a record to the list");
                     Console.WriteLine("1. View all the records in the list");
                     Console.WriteLine("2. Search for a record in the list");
                     Console.WriteLine("3. Display the first record in the list");
@@ -82,6 +130,11 @@ namespace exercise_3
                     char ch = Convert.ToChar(Console.ReadLine());
                     switch (ch)
                     {
+                        case '5':
+                            {
+                                obj.addNode();
+                            }
+                            break;
                         case '1':
                             {
                                 obj.traverse();
