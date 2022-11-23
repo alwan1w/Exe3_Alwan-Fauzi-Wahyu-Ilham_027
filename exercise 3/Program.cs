95714de279ae47c4738dea929f2bbae301fc7b26
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-11-23 15:44:15 +0700

    add class and creat method circularlist

--- exercise 3/Program.cs
+++ exercise 3/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace exercise_3
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
         {
+            LAST = null;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
+        {
+            for(previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if(rollNo == current.rollNumber)
+                    return (true);/*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*returns false if the not found*/
+                return true;
+            else
+                return (false);/*returns true if the node is present at the end*/
         }
     }
 }
